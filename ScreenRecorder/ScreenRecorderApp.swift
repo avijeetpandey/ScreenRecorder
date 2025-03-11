28b4dac25409c7c5afd6e51980c3bd2ffda8468a
Avijeet <avijeetpandey25@gmail.com>
2025-03-11 20:43:49 +0530

    Initial Commit

---
+++ ScreenRecorder/ScreenRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenRecorderApp.swift
+//  ScreenRecorder
+//
+//  Created by Avijeet Pandey on 11/03/25.
+//
+
+import SwiftUI
+
+@main
+struct ScreenRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScreenRecorderTests/ScreenRecorderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenRecorderTests.swift
+//  ScreenRecorderTests
+//
+//  Created by Avijeet Pandey on 11/03/25.
+//
+
+import Testing
+@testable import ScreenRecorder
+
+struct ScreenRecorderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
